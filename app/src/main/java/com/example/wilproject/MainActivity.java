485c7e5f26bd30cdcd848c3e1612d521ae7c14f3
diljit7686 <diljit7686@gmail.com>
2020-06-03 14:06:47 -0400

    added sign up page with new activity from signup Button

--- app/src/main/java/com/example/wilproject/MainActivity.java
+++ app/src/main/java/com/example/wilproject/MainActivity.java
@@ -7,30 +7,53 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button;
+    Button mainMenuButton , signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainMenuButton = (Button) findViewById(R.id.button);
+
+        signUpButton = (Button) findViewById(R.id.signUpBtnID);
 
 
-
-
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewActivity();
-            }
-        });
+        mainMenuButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+//
+//        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openNewActivity();
+//            }
+//        });
+//    }
+//    public void openNewActivity(){
+//        Intent mainMenuIntent = new Intent(this, navigationActivity.class);
+//        startActivity(mainMenuIntent);
+//
+////        Intent signUpIntent = new Intent(this, signUpActivity.class);
+////        startActivity(signUpIntent);
+//    }
     }
-    public void openNewActivity(){
-        Intent intent = new Intent(this, navigationActivity.class);
-        startActivity(intent);
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button:
+                Intent intent = new Intent(this,navigationActivity.class);
+                startActivity(intent);
+                break;
+
+
+                case R.id.signUpBtnID:
+                    Intent intent2 = new Intent(this,signUpActivity.class);
+                    startActivity(intent2);
+                    break;
+        }
+
     }
-
-
 }
